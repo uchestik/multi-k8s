f9c065090b8ffe7881afa6997d172fe92650562d
Uchenna Nwankpa <uchenna_nwankpa@yahoo.com>
2021-11-25 21:18:07 -0600

    finalized travis file

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t uchestik/fib-client:latest -t uchestik/fib-client:$SHA -f ./frontend/Dockerfile ./frontend
+docker build -t uchestik/fib-server:latest -t uchestik/fib-server:$SHA -f ./server/Dockerfile ./server
+docker build -t uchestik/fib-worker:latest -t uchestik/fib-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push uchestik/fib-client:latest
+docker push uchestik/fib-server:latest
+docker push uchestik/fib-worker:latest
+
+docker push uchestik/fib-client:$SHA
+docker push uchestik/fib-server:$SHA
+docker push uchestik/fib-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=uchestik/fib-server:$SHA
+kubectl set image deployments/client-deployment client=uchestik/fib-client:$SHA
+kubectl set image deployments/worker-deployment worker=uchestik/fib-worker:$SHA
